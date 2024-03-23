debcd8ee849ce3233a472187d5e7f51a6cb794a3
Jakub Kozlowski <jakub.kozlowski1104@gmail.com>
2024-03-23 16:33:17 +0100

    inicial back commit

--- moviesAppBackend/src/main/java/com/kuba/moviesApp/controller/UserController.java
+++ moviesAppBackend/src/main/java/com/kuba/moviesApp/controller/UserController.java
@@ -5,19 +5,30 @@ import com.kuba.moviesApp.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 
 @RestController
+@CrossOrigin("*")
 public class UserController {
 
     @Autowired
     private UserRepository userRepository;
+
+
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userRepository.findAll();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
 
     @PostMapping("/user/register/check")
     public ResponseEntity<Map<String, Object>> canSignUp(@RequestBody User newUser) {
@@ -28,15 +39,15 @@ public class UserController {
         if (emailExists && usernameExists) {
             response.put("status", 1);
             response.put("message", "Email and username already exist");
-            return new ResponseEntity<>(response, HttpStatus.CONFLICT);
+            return new ResponseEntity<>(response, HttpStatus.OK);
         } else if (emailExists) {
             response.put("status", 2);
             response.put("message", "Email already exists");
-            return new ResponseEntity<>(response, HttpStatus.CONFLICT);
+            return new ResponseEntity<>(response, HttpStatus.OK);
         } else if (usernameExists) {
             response.put("status", 3);
             response.put("message", "Username already exists");
-            return new ResponseEntity<>(response, HttpStatus.CONFLICT);
+            return new ResponseEntity<>(response, HttpStatus.OK);
         } else {
             userRepository.save(newUser);
             response.put("status", 0);
