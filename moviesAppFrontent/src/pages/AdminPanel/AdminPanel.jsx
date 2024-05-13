25f5d0e0c509664442b240707e0edacf7c0fde5c
Jakub Kozlowski <jakub.kozlowski1104@gmail.com>
2024-05-13 12:32:55 +0200

    add update user

--- moviesAppFrontent/src/pages/AdminPanel/AdminPanel.jsx
+++ moviesAppFrontent/src/pages/AdminPanel/AdminPanel.jsx
@@ -1,8 +1,40 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import { StyledCenter } from './AdminPanel.styles';
+import Modal from 'react-modal';
 
 const AdminPanel = () => {
   const [users, setUsers] = useState([]);
+  const [selectedUser, setSelectedUser] = useState(null);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [updatedUserData, setUpdatedUserData] = useState({});
+
+  // Funkcja do otwierania modalu edycji
+  const openEditModal = (user) => {
+    setSelectedUser(user);
+    setUpdatedUserData(user); // Ustawienie danych użytkownika w stanie początkowym formularza edycji
+    setModalIsOpen(true);
+  };
+
+  // Funkcja do zamykania modalu edycji
+  const closeEditModal = () => {
+    setSelectedUser(null);
+    setModalIsOpen(false);
+    setUpdatedUserData({}); // Zresetowanie danych użytkownika po zamknięciu modalu
+  };
+
+  const handleEditUser = async () => {
+    try {
+      await axios.post(`/api/user/update/${selectedUser.id}`, updatedUserData);
+      // Pobranie zaktualizowanych danych użytkownika z API
+      const response = await axios.get('/api/users');
+      setUsers(response.data);
+      closeEditModal(); // Zamknięcie modalu po zakończeniu edycji
+      alert(`User with id ${selectedUser.id} has been updated`);
+    } catch (error) {
+      console.error('Error updating user:', error);
+    }
+  };
 
   useEffect(() => {
     const fetchUsers = async () => {
@@ -28,7 +60,7 @@ const AdminPanel = () => {
   };
 
   return (
-    <div>
+    <StyledCenter>
       <h1>Admin Panel</h1>
       <table>
         <thead>
@@ -37,7 +69,7 @@ const AdminPanel = () => {
             <th>Email</th>
             <th>Password</th>
             <th>isAdmin</th>
-            <th>Action</th>
+            <th>Actions</th>
           </tr>
         </thead>
         <tbody>
@@ -51,12 +83,64 @@ const AdminPanel = () => {
                 <button onClick={() => handleDeleteUser(user.id)}>
                   Delete
                 </button>
+                <button onClick={() => openEditModal(user)}>Update</button>
               </td>
             </tr>
           ))}
         </tbody>
       </table>
-    </div>
+
+      <Modal isOpen={modalIsOpen} onRequestClose={closeEditModal}>
+        <h2>Edit User</h2>
+        <form>
+          <label>
+            Name:
+            <input
+              type="text"
+              value={updatedUserData.username}
+              onChange={(e) =>
+                setUpdatedUserData({
+                  ...updatedUserData,
+                  username: e.target.value,
+                })
+              }
+            />
+          </label>
+          <label>
+            Email:
+            <input
+              type="text"
+              value={updatedUserData.email}
+              onChange={(e) =>
+                setUpdatedUserData({
+                  ...updatedUserData,
+                  email: e.target.value,
+                })
+              }
+            />
+          </label>
+          <label>
+            Password:
+            <input
+              type="text"
+              value={updatedUserData.password}
+              onChange={(e) =>
+                setUpdatedUserData({
+                  ...updatedUserData,
+                  password: e.target.value,
+                })
+              }
+            />
+          </label>
+          <button type="button" onClick={handleEditUser}>
+            Save
+          </button>
+          <button type="button" onClick={closeEditModal}>
+            Cancel
+          </button>
+        </form>
+      </Modal>
+    </StyledCenter>
   );
 };
 
